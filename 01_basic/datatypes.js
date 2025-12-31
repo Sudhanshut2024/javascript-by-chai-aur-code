1d8ff5b988042c23a83599eff53618e2ed9a47ca
Sudhanshut2024 <sudhanshutripathi900@gmail.com>
2025-12-31 07:44:29 +0000

    variable and its Datatypes Done

---
+++ 01_basic/datatypes.js
@@ -0,0 +1,8 @@
+let FirstName = "Sudhanshu"
+let age = 22
+let Height = 40.5
+let isLoggedIn = true
+let notdef;
+let nullobj = null
+
+console.log(typeof (nullobj));
---
+++ 01_basic/vairable.js
@@ -0,0 +1,15 @@
+const MyNumber = 121225;  // This is Fix Final value Can't Chane it is Constant Value
+
+var FirstName = "Sudhanshu";  // now days not in use cause of Block Scope problem it was override instance variable
+
+let LastName = "Tripathi";  // nowdays we use this support block and scoping
+
+let city;  // in js this is a undefined variables
+
+console.log(FirstName);
+
+console.log(MyNumber, FirstName);
+
+console.table([MyNumber, FirstName, LastName]);  // i you want to print the multiple variables in tabular format 
+
+
